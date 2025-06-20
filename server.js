b69600903578b8b8f179cbc4d955e518cc0a7833
Anugya Verma <119095704+anugyaverma99@users.noreply.github.com>
2025-06-20 11:54:20 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express=require('express');
 
 const authRoutes=require('./src/routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app=express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+const corsOptions={
+    origin:'http://localhost:3000',
+    credentials:true
+};
+
+app.use(cors(corsOptions));
 
 app.use('/auth',authRoutes);
 
